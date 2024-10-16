31e2f4cfb72b11e69314b02e68bcc8585f55e4f5
shanmu <76567427+shanmu-ss@users.noreply.github.com>
2024-10-16 18:27:30 +0530

    Update HomeHeader.jsx
    Home Page changes

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -1,36 +1,29 @@
-import React from 'react'
-import HomePortfolio from '../components/HomePortfolio'
+// HomeHeader.js
+import React from 'react';
+import '../Portfolio.css';
+import HomePortfolio from '../components/HomePortfolio';
 
 function HomeHeader() {
   return (
-    <div>
-      <div>
-        <HomePortfolio />
-        <br /> <br />
-        <div className="container-fluid bg-dark d-flex align-items-start mb-5 py-5" id="home" style={{ minHeight: '100vh', display: 'flex' }}>
-          <div className="container" style={{ flex: 1 , marginRight: '20px' }}>
-            <div className="row align-items-center">
-              <div className="col-lg-7 text-center text-lg-left">
-              <h3 className="text-white display-2 font-weight-normal pt-5 mb-3">Hi, This is <span className="kode-mono-shanmu">SHANMU</span></h3>
-                
-                <h1 className="d-inline text-md-left font-weight-normal text-white">Associate Software Engineer</h1>
-                <p className='text-white pt-3 mb-3'>Experienced Cloud Engineer with 2+ years in both professional and freelance settings. Dedicated to ongoing learning, I actively pursue skill improvement and stay current with the latest in Technology and DevOps. Proven track record of driving successful projects through proactive research and collaboration, fostering organizational growth.</p>
-                <div className="d-none"></div>
-                <div className="d-flex align-items-center justify-content-center pt-3">
-                  <a href="doc/Resume.pdf" className=" btn btn-secondary mr-4">Download Resume</a>
-                  {/* Add other buttons or links here */}
-                </div>
-                <br /> <br />
-              </div>
-              <div className="col-lg-6 px-8 pl-lg-1 pb-5 pb-lg-6 about-img" style={{ flex: 1, marginLeft: 'auto', marginRight: -300 }}>
-                <img className="img-fluid w-50 rounded-circle shadow-sm" src="https://i.ibb.co/gwTZd6m/Untitled-design.png" alt="profile-image" />
-              </div>
-            </div>
+    <div className="portfolio-section">
+      <HomePortfolio />
+      <div className="container text-white py-5">
+        <div className="row align-items-center">
+          <div className="col-lg-7">
+            <h3 className="display-4">Hi, This is <span className="highlight">SHANMU</span></h3>
+            <h1 className="font-weight-normal">Associate Software Engineer</h1>
+            <p className="mt-3">
+              Experienced Cloud Engineer with 2+ years in both professional and freelance settings. Dedicated to ongoing learning, I actively pursue skill improvement and stay current with the latest in Technology and DevOps.
+            </p>
+            <a href="doc/Resume.pdf" className="btn btn-secondary">Download Resume</a>
+          </div>
+          <div className="col-lg-5 text-center">
+            <img src="img/shanmu.png" className="img-fluid rounded-circle" alt="Profile" />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default HomeHeader
+export default HomeHeader;
