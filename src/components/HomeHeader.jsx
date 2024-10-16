8ae6dcda44fa961a8db18383325ac3117e4e19a5
shanmu <76567427+shanmu-ss@users.noreply.github.com>
2024-10-16 20:55:52 +0530

    changes in NavBar, Font changes in reach out button and Download Resume moved to canva

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -11,14 +11,15 @@ function HomeHeader() {
         <div className="row align-items-center">
           <div className="col-lg-7">
             <h3 className="display-4">Hi, This is <span className="highlight">SHANMU</span></h3>
-            <h1 className="font-weight-normal">Associate Software Engineer</h1>
+            <h1 className="font-weight-normal">Software Engineer</h1>
             <p className="mt-3">
               Experienced Cloud Engineer with 2+ years in both professional and freelance settings. Dedicated to ongoing learning, I actively pursue skill improvement and stay current with the latest in Technology and DevOps.
             </p>
-            <a href="doc/Resume.pdf" className="btn btn-secondary">Download Resume</a>
+            <a href="https://www.canva.com/design/DAFxI7GaMrM/C9Yvk4KLhe2ThyG6EiG-dw/view" className="btn btn-secondary">Download Resume</a>
           </div>
           <div className="col-lg-5 text-center">
-            <img src="https://i.ibb.co/gwTZd6m/Untitled-design.png" className="img-fluid rounded-circle" alt="profile-image" />
+            <img src="img/shanmu.png" className="img-fluid rounded-circle" alt="Profile" />
+            
           </div>
         </div>
       </div>
--- src/components/HomePortfolio.jsx
+++ src/components/HomePortfolio.jsx
@@ -1,4 +1,3 @@
-// Navbar.js
 import React from 'react';
 import '../Navbar.css';
 
@@ -6,19 +5,22 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-secondary fixed-top shadow-sm">
       <div className="container">
-        <a href="/" className="navbar-brand">
-          <h1 className="display-5 text-white">Shanmu</h1>
-        </a>
+        {/* <a href="/" className="navbar-brand">
+          <h1 className="home-portfolio-link">PORTFOLIO</h1>
+        </a> */}
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav ml-auto">
+            {/* <li className="nav-item">
+              <a href="#home-portfolio" className="nav-link">Home Portfolio</a>
+            </li> */}
             <li className="nav-item">
-              <a href="https://www.instagram.com/shanmu_throttler" className="nav-link text-white">Instagram</a>
+              <a href="https://www.instagram.com/shanmu_throttler" className="nav-link">Instagram</a>
             </li>
             <li className="nav-item">
-              <a href="https://www.linkedin.com/in/bs5" className="nav-link text-white">LinkedIn</a>
+              <a href="https://www.linkedin.com/in/bs5" className="nav-link">LinkedIn</a>
             </li>
             <li className="nav-item">
-              <a href="https://github.com/shanmu-ss" className="nav-link text-white">Github</a>
+              <a href="https://github.com/shanmu-ss" className="nav-link">Github</a>
             </li>
           </ul>
         </div>
